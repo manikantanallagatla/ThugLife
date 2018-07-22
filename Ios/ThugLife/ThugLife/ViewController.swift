632fda1493a1fdf6504a803e629dec6cd8fb98b8
Nallagatla Manikanta <manikant@adobe.com>
2018-07-22 18:58:52 +0530

    Thuglife app for ios and android

---
+++ Ios/ThugLife/ThugLife/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ThugLife
+//
+//  Created by Manikanta Nallagatla on 22/07/18.
+//  Copyright © 2018 Manikanta Nallagatla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		super.view.backgroundColor = UIColor.black
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
